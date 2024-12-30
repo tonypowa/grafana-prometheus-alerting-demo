2cfc69daf417853310a3035cf29eab391ed44ba8
tonypowa <tonypowa@gmail.com>
2024-12-30 16:38:46 +0100

    app

--- flask-app/app.py
+++ flask-app/app.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
     # Start Prometheus metrics server
     start_http_server(5000)
     # Run the Flask app
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
